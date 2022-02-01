1e274f51547a179aa7598f1112bf7c3bd8f848a2
0x4F776C <41299140+0x4F776C@users.noreply.github.com>
2022-02-01 11:18:04 +0800

    Add files via upload

---
+++ Java/test1.java
@@ -0,0 +1,12 @@
+// https://wiki.vg/Protocol#Without_compression
+// https://www.tutorialspoint.com/websockets/index.htm
+// https://stackoverflow.com/questions/30768091/java-sending-handshake-packets-to-minecraft-server
+
+// import
+import java.io.IOException;
+
+public class test1 {
+    private static void main(String[] args) throws IOException {
+
+    }
+}
